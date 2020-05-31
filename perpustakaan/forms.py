1bc2ef5665c65bceef113b33e667a3178ff007c2
Zul Hilmi <netspytux@gmail.com>
2020-05-31 21:55:22 +0700

    update episode: 13.2

--- perpustakaan/forms.py
+++ perpustakaan/forms.py
@@ -1,7 +1,16 @@
 from django.forms import ModelForm
+from django import forms
 from perpustakaan.models import Buku
 
 class FormBuku(ModelForm):
   class Meta:
     model = Buku
     exclude = ['penerbit']
+
+    widgets = {
+      'judul' : forms.TextInput({'class':'form-control'}),
+      'penulis' : forms.TextInput({'class':'form-control'}),
+      'penerbit' : forms.TextInput({'class':'form-control'}),
+      'jumlah' : forms.NumberInput({'class':'form-control'}),
+      'kelompok_id' : forms.Select({'class':'form-control'}),
+    }
