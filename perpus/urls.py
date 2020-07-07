996a0c094a1ff6f34a4828a80b3851d97091a2a0
Zul Hilmi <netspytux@gmail.com>
2020-07-07 20:53:48 +0700

    re-path url patterns

--- perpus/urls.py
+++ perpus/urls.py
@@ -6,14 +6,14 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('buku/', buku, name='buku'),
     path('penerbit/', penerbit, name='penerbit'),
-    path('tambah-buku/', tambah_buku, name='tambah_buku'),
+    path('buku/', buku, name='buku'),
+    path('buku/tambah/', tambah_buku, name='tambah_buku'),
     path('buku/ubah/<int:id_buku>', ubah_buku, name='ubah_buku'),
     path('buku/hapus/<int:id_buku>', hapus_buku, name="hapus_buku"),
-    path('masuk/', LoginView.as_view(), name='masuk'),
-    path('keluar/', LogoutView.as_view(next_page='masuk'), name='keluar'),
-    path('signup/', signup, name='signup'),
+    path('auth/masuk/', LoginView.as_view(), name='masuk'),
+    path('auth/keluar/', LogoutView.as_view(next_page='masuk'), name='keluar'),
+    path('user/add/', signup, name='signup'),
     path('export/xls/', export_xls, name='export_xls'),
 ]
 
