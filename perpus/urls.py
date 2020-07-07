4678fbda21e518f0357d4ac7deaf5566fb7b8be6
Zul Hilmi <netspytux@gmail.com>
2020-07-07 20:55:17 +0700

    re-path url patterns

--- perpus/urls.py
+++ perpus/urls.py
@@ -11,10 +11,10 @@ urlpatterns = [
     path('buku/tambah/', tambah_buku, name='tambah_buku'),
     path('buku/ubah/<int:id_buku>', ubah_buku, name='ubah_buku'),
     path('buku/hapus/<int:id_buku>', hapus_buku, name="hapus_buku"),
+    path('buku/export/xls/', export_xls, name='export_xls'),
     path('auth/masuk/', LoginView.as_view(), name='masuk'),
     path('auth/keluar/', LogoutView.as_view(next_page='masuk'), name='keluar'),
     path('user/add/', signup, name='signup'),
-    path('export/xls/', export_xls, name='export_xls'),
 ]
 
 if settings.DEBUG:
